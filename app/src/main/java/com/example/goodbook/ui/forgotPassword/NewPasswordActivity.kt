fc5a20e887caf80e5a23992533f3c6067736caaf
ttchan1521 <tranthutrang15012001@gmail.com>
2022-04-25 14:29:07 +0700

    create file activity

---
+++ app/src/main/java/com/example/goodbook/ui/forgotPassword/NewPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.goodbook.ui.forgotPassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.goodbook.R
+
+class NewPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+    }
+}
