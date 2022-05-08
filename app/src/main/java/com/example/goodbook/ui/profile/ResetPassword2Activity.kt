61fe5f2a123ef230957851af7d77d85ffc7fe91b
tam <vuthitam14082001@gmail.com>
2022-05-08 11:25:23 +0700

    add layouts

---
+++ app/src/main/java/com/example/goodbook/ui/profile/ResetPassword2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.goodbook.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.goodbook.R
+
+class ResetPassword2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password2)
+    }
+}
