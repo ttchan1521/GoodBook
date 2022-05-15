eb668cb13cba154ac5394fe57095efcf3278e772
tam <vuthitam14082001@gmail.com>
2022-05-15 10:19:58 +0700

    fix

--- app/src/main/java/com/example/goodbook/ui/profile/PasswordAuthentication.kt
+++ app/src/main/java/com/example/goodbook/ui/profile/PasswordAuthentication.kt
@@ -2,6 +2,7 @@ package com.example.goodbook.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.goodbook.R
 
 class PasswordAuthentication : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
