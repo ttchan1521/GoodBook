dfeba1667aa1ed5e45d141f0d9e980f90b4d2bc1
tam <vuthitam14082001@gmail.com>
2022-05-18 01:32:14 +0700

    intent

--- app/src/main/java/com/example/goodbook/ui/profile/ResetPassword1Activity.kt
+++ app/src/main/java/com/example/goodbook/ui/profile/ResetPassword1Activity.kt
@@ -8,5 +8,9 @@ class ResetPassword1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password1)
+
+        supportActionBar!!.title = "Đặt lại mật khẩu"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setBackgroundDrawable(resources.getDrawable(R.drawable.shape2))
     }
 }
--- app/src/main/java/com/example/goodbook/ui/profile/SavePostActivity.kt
+++ app/src/main/java/com/example/goodbook/ui/profile/SavePostActivity.kt
@@ -8,5 +8,9 @@ class SavePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_save_post)
+
+        supportActionBar!!.title = "Đã lưu"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setBackgroundDrawable(resources.getDrawable(R.drawable.shape2))
     }
 }
