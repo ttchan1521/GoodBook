55a03b50d9a5654977b2a88a96b4844d39ba9a58
Nhu Quynh <60933291+tanaquynh@users.noreply.github.com>
2022-05-25 07:48:35 +0700

    .

--- app/src/main/java/com/example/goodbook/ui/forgotPassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/goodbook/ui/forgotPassword/ForgotPasswordActivity.kt
@@ -1,16 +1,44 @@
 package com.example.goodbook.ui.forgotPassword
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.goodbook.GoodBookApplication
 import com.example.goodbook.R
+import com.example.goodbook.databinding.ActivityForgotPasswordBinding
+import com.example.goodbook.databinding.ActivityLoginBinding
+import com.example.goodbook.model.User
 import com.example.goodbook.ui.LoginActivity
+import com.example.goodbook.ui.MainActivity
+import com.example.goodbook.ui.RegisterActivity
+import com.example.goodbook.ui.viewmodel.LoginViewModel
+import com.example.goodbook.ui.viewmodel.LoginViewModelFactory
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val viewModel: LoginViewModel by viewModels() {
+        LoginViewModelFactory(
+            (application as GoodBookApplication).database
+                .goodBookDao()
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        val binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val allusers = viewModel.allUsers.observe(this) {
+            Log.d(ContentValues.TAG, "viewModel.allUsers.observe is called")
+        }
     }
 
     fun confirmAction(view: View) {
--- app/src/main/java/com/example/goodbook/ui/forgotPassword/NewPasswordActivity.kt
+++ app/src/main/java/com/example/goodbook/ui/forgotPassword/NewPasswordActivity.kt
@@ -1,12 +1,51 @@
 package com.example.goodbook.ui.forgotPassword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.goodbook.GoodBookApplication
 import com.example.goodbook.R
+import com.example.goodbook.databinding.ActivityForgotPasswordBinding
+import com.example.goodbook.databinding.ActivityNewPasswordBinding
+import com.example.goodbook.ui.LoginActivity
+import com.example.goodbook.ui.viewmodel.LoginViewModel
+import com.example.goodbook.ui.viewmodel.LoginViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 
 class NewPasswordActivity : AppCompatActivity() {
+
+    private val viewModel: LoginViewModel by viewModels() {
+        LoginViewModelFactory(
+            (application as GoodBookApplication).database
+                .goodBookDao()
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_password)
+        val binding = ActivityNewPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
+    }
+
+    fun successChangePass() {
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun failLogin() {
+        showSnackbar()
+    }
+
+    private fun showSnackbar(): Boolean {
+        val squeezeText = "Thay đổi mật khẩu thất bại"
+        Snackbar.make(
+            findViewById(R.id.forgot_pass_layout),
+            squeezeText,
+            Snackbar.LENGTH_SHORT
+        ).show()
+        return true
     }
 }
